c17a45c8807a33e5db3bb90739f94d5972151c7d
Artur <a.sincarenco.97@gmail.com>
2022-05-06 12:41:13 +0300

    feat: pop and shift update

--- solo-project-my-emojis/index.js
+++ solo-project-my-emojis/index.js
@@ -1,7 +1,8 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-const emojiContainer = document.getElementById("emoji-container")
 
 function renderEmojis() {
+    const emojiContainer = document.getElementById("emoji-container")
+    emojiContainer.innerHTML = ""
     for (let i = 0; i < myEmojis.length; i++) {
         const emoji = document.createElement('span')
         emoji.textContent = myEmojis[i]
@@ -17,7 +18,28 @@ pushBtn.addEventListener("click", function(){
     if (emojiInput.value) {
         myEmojis.push(emojiInput.value)
         emojiInput.value = ""
-        emojiContainer.innerHTML = ""
         renderEmojis()
     }
+})
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+const popBtn = document.getElementById("pop-btn")
+popBtn.addEventListener("click", function(){
+    myEmojis.pop()
+    renderEmojis()
+})
+
+const shiftBtn = document.getElementById("shift-btn")
+shiftBtn.addEventListener("click", function(){
+    myEmojis.shift()
+    renderEmojis()
 })
