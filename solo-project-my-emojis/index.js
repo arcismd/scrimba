14b534a5130a239934a079f981230ad0ff4f00a0
Artur <a.sincarenco.97@gmail.com>
2022-05-06 12:16:12 +0300

    feat: adding the input and button

--- solo-project-my-emojis/index.js
+++ solo-project-my-emojis/index.js
@@ -1,9 +1,14 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-const emojiContainer = document.getElementById("emojiContainer")
+const emojiContainer = document.getElementById("emoji-container")
 
 for (let i = 0; i < myEmojis.length; i++) {
-    // write your code here
     const emoji = document.createElement('span')
     emoji.textContent = myEmojis[i]
     emojiContainer.append(emoji)
 }
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput.value)
+})
