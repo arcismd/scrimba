490972e594220cf1495acc602ed5aa2cf421fde2
Artur <a.sincarenco.97@gmail.com>
2022-05-07 21:26:37 +0300

    feat: add DOM acces

---
+++ solo-project-invoice-creator/index.js
@@ -0,0 +1,7 @@
+const washCarEl = document.getElementById("washCar-el") 
+const mowLawNeL = document.getElementById("mowLawn-el") 
+const pullWeeds = document.getElementById("pullWeeds-el") 
+let sendEl = document.getElementById("send-el")
+let serviceTextEl = document.getElementById("serviceText-el")
+let sumEl = document.getElementById("sum-el")
+let sum = 0
