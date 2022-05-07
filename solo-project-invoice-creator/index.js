333b3846ed77328beb9e1a4d298b751518eab26d
Artur <a.sincarenco.97@gmail.com>
2022-05-07 21:27:26 +0300

    feat: add service and price function

--- solo-project-invoice-creator/index.js
+++ solo-project-invoice-creator/index.js
@@ -5,3 +5,31 @@ let sendEl = document.getElementById("send-el")
 let serviceTextEl = document.getElementById("serviceText-el")
 let sumEl = document.getElementById("sum-el")
 let sum = 0
+
+function addService(id, service, price) {
+    id.addEventListener('click', function() {
+        sum += price
+        serviceTextEl.innerHTML += 
+        `
+        <div class="services-text">
+            <div class="left">
+            ${service}
+            </div>
+            <div class="right">
+            $<span>${price}</span>
+            </div>
+        </div>
+        `
+        sumEl.innerHTML = 
+        `
+        <div class="third-text">
+            <div class="left">
+            We accept cash, credit card, or PayPal
+            </div>
+            <div class="right">
+            $${sum}
+            </div>
+        </div>
+        `
+    }, {once : true})
+}
