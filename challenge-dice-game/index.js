a6bbde05a56a199cbd2b6de6f9b92c02e4a5bf06
Artur <a.sincarenco.97@gmail.com>
2022-05-14 13:21:56 +0300

    feat: make the game fair

--- challenge-dice-game/index.js
+++ challenge-dice-game/index.js
@@ -1,6 +1,8 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+let player1Moves = 0
+let player2Moves = 0
 
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
@@ -9,8 +11,10 @@ const player2Scoreboard = document.getElementById("player2Scoreboard")
 const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
+const player1MovesEl = document.getElementById("player1moves")
+const player2MovesEl = document.getElementById("player2moves")
 
-function showDisplayButton() {
+function showResetButton() {
     rollBtn.style.display = "none"
     resetBtn.style.display = "block"
 }
@@ -21,6 +25,8 @@ rollBtn.addEventListener("click", function() {
     if (player1Turn) {
         player1Score += randomNumber
         player1Scoreboard.textContent = player1Score
+        player1Moves += 1
+        player1MovesEl.textContent = player1Moves
         player1Dice.textContent = randomNumber
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
@@ -28,37 +34,44 @@ rollBtn.addEventListener("click", function() {
     } else {
         player2Score += randomNumber
         player2Scoreboard.textContent = player2Score
+        player2Moves += 1
+        player2MovesEl.textContent = player2Moves
         player2Dice.textContent = randomNumber
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
         message.textContent = "Player 1 Turn"
     }
     
-    
-    if (player1Score >= 20) {
-        message.textContent = "Player 1 has won! 🥳"
-        showDisplayButton()
-    } else if (player2Score >= 20) {
-        message.textContent = "Player 2 has won! 🎉"
-        showDisplayButton()
+    if (player1Score >= 20 && player2Moves === player1Moves && player1Score > player2Score) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    }  else if (player2Score >= 20 && player2Score > player1Score) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    } else if (player1Score >= 20 && player2Score >= 20 && player1Score === player2Score){
+       message.textContent = "Equal Result"
+       showResetButton() 
     }
-    
     player1Turn = !player1Turn
 })
-
+ 
 resetBtn.addEventListener("click", function(){
     reset()
 })
- 
+
 function reset() {
-    message.textContent = "Player 1 Turn"
-    player1Scoreboard.textContent = 0
-    player2Scoreboard.textContent = 0
-    player1Dice.textContent = '-'
-    player2Dice.textContent = '-'
     player1Score = 0
     player2Score = 0
+    player1Moves = 0
+    player2Moves = 0
+    player1MovesEl.textContent = 0
+    player2MovesEl.textContent = 0
     player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
     resetBtn.style.display = "none"
     rollBtn.style.display = "block"
     player2Dice.classList.remove("active")
