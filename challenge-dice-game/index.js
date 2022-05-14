51bed54f40ffca20ee1af0792fc781ea02e562f0
Artur <a.sincarenco.97@gmail.com>
2022-05-14 11:04:59 +0300

    feat: setting up the DOM nodes

--- challenge-dice-game/index.js
+++ challenge-dice-game/index.js
@@ -1,3 +1,11 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
