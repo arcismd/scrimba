5c6b1bb9c2eefcdaa450a9ba871ef6e482ec9c59
Artur <a.sincarenco.97@gmail.com>
2022-05-17 11:34:06 +0300

    feat: modify renderCharacter()

--- role-playing-game/index.js
+++ role-playing-game/index.js
@@ -1,24 +1,33 @@
-const heroElementId = "hero"
-const heroName = "Wizard"
-const heroAvatar = "images/wizard.png"
-const heroHealth = "60"
-const heroDiceRoll = 6
-
-const monsterElementId = "monster"
-const monsterName = "Orc"
-const monsterAvatar = "images/orc.png"
-const monsterHealth = "10"
-const monsterDiceRoll = 4
-
-function renderCharacter(elementId, name, avatar, health, diceRoll){
-  document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health}</b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
-    </div>`;   
+const hero = {
+    elementId: "hero", 
+    name: "Wizard", 
+    avatar: "images/wizard.png", 
+    health: 60, 
+    diceRoll: 6
 }
+
+const monster = {
+    elementId: "monster", 
+    name: "Orc", 
+    avatar: "images/orc.png", 
+    health: 10, 
+    diceRoll: 4
+}
+
+function renderCharacter(data) {
+    document.getElementById(data.elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${data.name} </h4>
+            <img class="avatar" src="${data.avatar}" />
+            <div class="health">health: <b> ${data.health} </b></div>
+            <div class="dice-container">
+                <div class="dice"> ${data.diceRoll} </div>
+            </div>
+        </div>`
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
 
 renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
 renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll)
