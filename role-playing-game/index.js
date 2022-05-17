3a4e1997d0b10f2d9b649499be1cb67b98b7f690
Artur <a.sincarenco.97@gmail.com>
2022-05-17 11:48:03 +0300

    feat: object destructing

--- role-playing-game/index.js
+++ role-playing-game/index.js
@@ -1,33 +1,35 @@
 const hero = {
-    elementId: "hero", 
-    name: "Wizard", 
-    avatar: "images/wizard.png", 
-    health: 60, 
-    diceRoll: 6
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 3,
 }
 
 const monster = {
-    elementId: "monster", 
-    name: "Orc", 
-    avatar: "images/orc.png", 
-    health: 10, 
-    diceRoll: 4
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4,
 }
 
+// CHALLENGE
+// 1. Deconstruct the data object 
+// 2. Update the template string as necessary
+
 function renderCharacter(data) {
-    document.getElementById(data.elementId).innerHTML =
+    const {elementId, name, avatar, health, diceRoll} = data
+    document.getElementById(elementId).innerHTML =
         `<div class="character-card">
-            <h4 class="name"> ${data.name} </h4>
-            <img class="avatar" src="${data.avatar}" />
-            <div class="health">health: <b> ${data.health} </b></div>
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
             <div class="dice-container">
-                <div class="dice"> ${data.diceRoll} </div>
+                <div class="dice"> ${diceRoll} </div>
             </div>
         </div>`
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
-
-renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
-renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll)
+renderCharacter(hero)
+renderCharacter(monster)
