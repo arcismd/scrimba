07a301c1ca8fe0da7214cbd9a685df2ef268907b
Artur <a.sincarenco.97@gmail.com>
2022-06-16 00:08:37 +0300

    docs: import project from scrim editor

--- role-playing-game/data.js
+++ role-playing-game/data.js
@@ -1,17 +1,31 @@
 const characterData = {
     hero: {
-        elementId: "hero",
         name: "Wizard",
         avatar: "images/wizard.png",
         health: 60,
-        diceCount: 3
+        diceCount: 3,
+        currentDiceScore: []
     },
-    monster: {
-        elementId: "monster",
+    orc: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
-        diceCount: 1
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     }
 }
 
