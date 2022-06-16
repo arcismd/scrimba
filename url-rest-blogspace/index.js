8cf3135ef14113190dbaa9bda5d3a71ad4761d7a
Artur <a.sincarenco.97@gmail.com>
2022-06-16 20:39:47 +0300

    feat: reset form

--- url-rest-blogspace/index.js
+++ url-rest-blogspace/index.js
@@ -1,4 +1,7 @@
 let postsArray = []
+const titleInput = document.getElementById("post-title")
+const bodyInput = document.getElementById("post-body")
+const form = document.getElementById("new-post")
 
 function renderPosts() {
     let html = ""
@@ -19,15 +22,15 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         renderPosts()
     })
 
-document.getElementById("new-post").addEventListener("submit", function (e) {
+form.addEventListener("submit", function(e) {
     e.preventDefault()
-    const postTitle = document.getElementById("post-title").value
-    const postBody = document.getElementById("post-body").value
+    const postTitle = titleInput.value
+    const postBody = bodyInput.value
     const data = {
         title: postTitle,
         body: postBody
     }
-
+    
     const options = {
         method: "POST",
         body: JSON.stringify(data),
@@ -35,11 +38,15 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
             "Content-Type": "application/json"
         }
     }
-
+    
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
         .then(post => {
             postsArray.unshift(post)
             renderPosts()
+            
+            titleInput.value = ""
+            bodyInput.value = ""
+            // form.reset()
         })
 })
