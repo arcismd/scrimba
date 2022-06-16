e721f2a6926a1207602997bbbaffc4b4504c8a11
Artur <a.sincarenco.97@gmail.com>
2022-06-16 18:20:38 +0300

    feat: styling using DOM element 'classList'

--- api-bored-bot/index.js
+++ api-bored-bot/index.js
@@ -3,5 +3,7 @@ document.getElementById("get-activity").addEventListener("click", function() {
       .then(response => response.json())
       .then(data => {
         document.getElementById("activity").textContent = data.activity
+        document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        document.body.classList.add("fun")
       })
   })
