01e959743811972aef350f237e5b76595e8bb37a
Artur <a.sincarenco.97@gmail.com>
2022-06-16 19:13:47 +0300

    feat: display blogs on page

---
+++ url-rest-blogspace/index.js
@@ -0,0 +1,14 @@
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
