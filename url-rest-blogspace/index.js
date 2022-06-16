ec970f13ebf16bb46f5f3f4d2d0fa192eefd8a4a
Artur <a.sincarenco.97@gmail.com>
2022-06-16 19:52:39 +0300

    feat: add new post to list of posts

--- url-rest-blogspace/index.js
+++ url-rest-blogspace/index.js
@@ -5,7 +5,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         let html = ""
         for (let post of postsArr) {
             html += `
-                <h3>${post.title}</h3>
+                <h3 class="blah">${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
             `
@@ -32,5 +32,12 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
 
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(post => {
+            document.getElementById("blog-list").innerHTML = `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("blog-list").innerHTML}
+            `
+        })
 })
