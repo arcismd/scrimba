2f0647c8c9203f5ffc37daea50db0c186c04f40d
Artur <a.sincarenco.97@gmail.com>
2022-06-16 20:33:27 +0300

    feat: posts refactor

--- url-rest-blogspace/index.js
+++ url-rest-blogspace/index.js
@@ -1,16 +1,22 @@
+let postsArray = []
+
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3 class="blah">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
 
 document.getElementById("new-post").addEventListener("submit", function (e) {
@@ -33,11 +39,7 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
         .then(post => {
-            document.getElementById("blog-list").innerHTML = `
-                <h3 class="blah">${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-                ${document.getElementById("blog-list").innerHTML}
-            `
+            postsArray.unshift(post)
+            renderPosts()
         })
 })
