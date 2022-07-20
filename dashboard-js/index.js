4c4c04860baaed115c42557e6c24898a8a029f00
Artur <a.sincarenco.97@gmail.com>
2022-07-20 19:01:12 +0300

    feat: add author info

--- dashboard-js/index.js
+++ dashboard-js/index.js
@@ -1,5 +1,6 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-.then(res => res.json())
-.then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
-})
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
