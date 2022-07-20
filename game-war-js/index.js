1aa3316223cd8220f27afae0152812b4b69778c0
Artur <a.sincarenco.97@gmail.com>
2022-07-20 17:42:29 +0300

    feat: add basic layout

---
+++ game-war-js/index.js
@@ -0,0 +1,12 @@
+let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
