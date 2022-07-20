bd3cb6561140ed82dabc0ed9a0b63ae8067c2e5d
Artur <a.sincarenco.97@gmail.com>
2022-07-20 18:12:12 +0300

    feat: display card images

--- game-war-js/index.js
+++ game-war-js/index.js
@@ -7,8 +7,18 @@ function handleClick() {
             console.log(data)
             deckId = data.deck_id
         })
-}document.getElementById('draw-cards').addEventListener('click', () => {
-    fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
-})
+}
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
+})
