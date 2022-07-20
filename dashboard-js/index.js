9309b3c1e0f55a29d42c5da67e35e36dbf4c1170
Artur <a.sincarenco.97@gmail.com>
2022-07-20 19:44:26 +0300

    feat: refactor

--- dashboard-js/index.js
+++ dashboard-js/index.js
@@ -43,8 +43,8 @@ navigator.geolocation.getCurrentPosition(position => {
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
-                <p>${Math.round(data.main.temp)}º</p>
-                <p>${data.name}</p>
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
             `
         })
         .catch(err => console.error(err))
