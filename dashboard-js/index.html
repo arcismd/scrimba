656f97fd87ba16c6290eb4c7652f67b026900ae1
Artur <a.sincarenco.97@gmail.com>
2022-07-20 18:59:49 +0300

    feat: add basic layout

---
+++ dashboard-js/index.html
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <script src="index.js"></script>
+    </body>
+</html>
---
+++ dashboard-js/index.js
@@ -0,0 +1,5 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+.then(res => res.json())
+.then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+})
